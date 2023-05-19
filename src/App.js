092d7a1fea2a0cfa44f178321f7b4e4859577271
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-05-20 03:16:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
